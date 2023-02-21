6d100b51172601bb4ae11f98df7a41d5444a234e
JayPokale <jay.pokale.35@gmail.com>
2023-02-21 15:28:00 +0530

    Responsive homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,13 +9,14 @@ const index = () => {
   return (
     <div class="max-w-6xl flex flex-col items-center mx-auto gap-12 pt-20">
       <div
-        class="flex flex-col items-center text-8xl font-bold tracking-tight"
+        class="flex flex-col items-center text-5xl xs:text-7xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tight"
         style={{ "font-family": "Signika" }}
       >
-        <p class="drop-shadow-md">EVERYTHING IS ABOUT</p>
-        <div class="relative w-96 h-24 py-8 overflow-hidden">
+        <p class="drop-shadow-md hidden sm:block">EVERYTHING IS ABOUT</p>
+        <p class="drop-shadow-md sm:hidden text-center">EVERYTHING<br/>IS ABOUT</p>
+        <div class="relative w-80 h-16 md:h-24 py-8 overflow-hidden">
           <div
-            class={`absolute top-2 h-24 w-80 grid place-items-center ${
+            class={`absolute top-0 h-16 md:h-24 w-80 grid place-items-center ${
               i() === 0
                 ? "translate-y-32"
                 : i() === 1
@@ -26,7 +27,7 @@ const index = () => {
             <div>THINK</div>
           </div>
           <div
-            class={`absolute top-2 h-24 w-80 grid place-items-center ${
+            class={`absolute top-0 h-16 md:h-24 w-80 grid place-items-center ${
               i() === 1
                 ? "translate-y-32"
                 : i() === 2
@@ -34,10 +35,10 @@ const index = () => {
                 : "-translate-y-32 duration-500"
             }`}
           >
-            <div>WRITE</div>
+            <div>CREATE</div>
           </div>
           <div
-            class={`absolute top-2 h-24 w-80 grid place-items-center ${
+            class={`absolute top-0 h-16 md:h-24 w-80 grid place-items-center ${
               i() === 2
                 ? "translate-y-32"
                 : i() === 0
@@ -56,7 +57,7 @@ const index = () => {
         sapiente eligendi sit perferendis. Voluptatibus voluptas quibusdam nihil
         autem!
       </h1>
-      <div class="flex gap-8">
+      <div class="flex gap-8 flex-col sm:flex-row">
         <button class="w-60 py-2 rounded-lg text-lg duration-100 shadow-md shadow-black/10 hover:shadow-lg bg-black text-white">
           Create Post
         </button>
@@ -64,17 +65,17 @@ const index = () => {
           Explore
         </button>
       </div>
-      <div class="flex w-full justify-evenly">
-        <button class="w-1/5 aspect-video rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
+      <div class="flex flex-wrap gap-8 justify-center w-full place-items-center">
+        <button class="w-full xs:w-52 aspect-[4/3] rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
           Container 1
         </button>
-        <button class="w-1/5 aspect-video rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
+        <button class="w-full xs:w-52 aspect-[4/3] rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
           Container 2
         </button>
-        <button class="w-1/5 aspect-video rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
+        <button class="w-full xs:w-52 aspect-[4/3] rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
           Container 3
         </button>
-        <button class="w-1/5 aspect-video rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
+        <button class="w-full xs:w-52 aspect-[4/3] rounded-xl shadow-md shadow-black/[0.02] hover:shadow-lg duration-300">
           Container 4
         </button>
       </div>
