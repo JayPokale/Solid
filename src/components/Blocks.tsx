3479b9f687c9c2e4cdd91d7451a3fef832e3ce8a
JayPokale <jay.pokale.35@gmail.com>
2023-02-25 13:22:32 +0530

    Cards Hover Effect

--- src/components/Blocks.tsx
+++ src/components/Blocks.tsx
@@ -2,15 +2,9 @@ import { createSignal } from "solid-js";
 import "./Blocks.css";
 
 const Blocks = () => {
-  const [mousePos, setMousePos] = createSignal<{ x: number; y: number }>({
-    x: 0,
-    y: 0,
-  });
-  const handleMouseMove = (e: MouseEvent) => {
-    setMousePos({ x: e.clientX, y: e.clientY });
-  };
   return (
     <div
+      id="cards"
       class="cards flex flex-wrap justify-center gap-2 mx-auto max-w-4xl"
       onMouseMove={(e) => {
         for (const card of document.getElementsByClassName(
@@ -19,16 +13,15 @@ const Blocks = () => {
           const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
-
           card.style.setProperty("--mouse-x", `${x}px`);
           card.style.setProperty("--mouse-y", `${y}px`);
         }
       }}
     >
-      <div class="card w-64 h-64 grid place-items-center rounded-lg border"></div>
-      <div class="card w-64 h-64 grid place-items-center rounded-lg border"></div>
-      <div class="card w-64 h-64 grid place-items-center rounded-lg border"></div>
-      <div class="card w-64 h-64 grid place-items-center rounded-lg border"></div>
+      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
+      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
+      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
+      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
     </div>
   );
 };
