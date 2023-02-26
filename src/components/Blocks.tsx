7cdc1a0657f1af7d5f94314abdbd630b6b30df3d
JayPokale <jay.pokale.35@gmail.com>
2023-02-26 16:01:16 +0530

    Cards Hover Effect

--- src/components/Blocks.tsx
+++ src/components/Blocks.tsx
@@ -1,14 +1,15 @@
-import { createSignal } from "solid-js";
+import { For } from "solid-js";
 import "./Blocks.css";
 
 const Blocks = () => {
+  const block = [1, 2, 3, 4];
   return (
     <div
       id="cards"
       class="cards flex flex-wrap justify-center gap-2 mx-auto max-w-4xl"
       onMouseMove={(e) => {
         for (const card of document.getElementsByClassName(
-          "card"
+          "cardborder"
         ) as HTMLCollectionOf<HTMLElement>) {
           const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
@@ -18,10 +19,13 @@ const Blocks = () => {
         }
       }}
     >
-      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
-      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
-      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
-      <div class="card max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 grid place-items-center rounded-lg border"></div>
+      <For each={block}>
+        {() => (
+          <div class="cardborder p-px grid place-items-center rounded-lg relative overflow-hidden max-w-[448px] min-w-[256px] xs:min-w-[320px] flex-1 h-80 2xs:h-96 bg-gray-50">
+            <div class="card relative overflow-hidden w-full h-full grid place-items-center rounded-lg bg-gray-50 z-10"></div>
+          </div>
+        )}
+      </For>
     </div>
   );
 };
