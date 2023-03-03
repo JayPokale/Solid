807c1ca82d6851a850dcdc1c874628f7857b6ee3
JayPokale <jay.pokale.35@gmail.com>
2023-03-03 11:51:31 +0530

    ProfileBasic

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,11 @@
-import ProfileHead from "~/components/ProfileHead";
+import ProfileBasic from "~/components/Profile/ProfileBasic";
+
 const profile = () => {
   return (
     <div class="max-w-screen-2xl mx-auto flex justify-center">
-      <ProfileHead />
+      <div class="w-full flex justify-center gap-4 my-4">
+        <ProfileBasic/>
+      </div>
     </div>
   );
 };
